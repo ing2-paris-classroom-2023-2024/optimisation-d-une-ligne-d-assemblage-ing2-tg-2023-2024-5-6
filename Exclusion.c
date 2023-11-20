7b57a9829ae449f59ee6212eef710331fa267aa3
Alara Tanguy <alara.tanguy@edu.ece.fr>
2023-11-20 23:27:01 +0100

    (Alara) Structure données + Lecture des fichiers de contrainte + Initialiation à la répartition des tâches + Système de vérification de la répartition des opérations + Attribution des la répartition des opérations dans les statons

--- Exclusion.c
+++ Exclusion.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 
 #define OPERATION_MAX 100
-#define NB_STATTIONS_MAX 10
+#define NB_STATIONS_MAX 10
 
 // Structure pour représenter les contraintes d'exclusion
 typedef struct
@@ -42,6 +42,56 @@ void LireFichierContraintes(char* nomFichier, Exclusion* exclusion, int* nbExclu
 
     fclose(fichier);
 }
+
+
+
+
+// INITIALISATION DE LA REPARTITION DES OPERATIONS SUR LES STATIONS
+
+void initialisationRepartition(Repartition* repartition)
+{
+    for (int i = 0; i < OPERATION_MAX; i++)
+    {
+        repartition->station[i] = -1;
+    }
+}
+
+
+// SYSTEME DE VERIFICATION DE LA REPARTITION DES OPERATIONS SUR LES STATIONS
+
+void verification (Repartition* repartition, Exclusion* exclusion, int nbExclusions)
+{
+    for (int i = 0; i < nbExclusions; i++)
+    {
+        if (repartition->station[exclusion[i].op1] == repartition->station[exclusion[i].op2])
+        {
+            printf("Erreur : les opérations %d et %d sont sur la même station\n", exclusion[i].op1, exclusion[i].op2);
+            exit(EXIT_FAILURE);
+        }
+    }
+}
+
+
+
+// ATTRIBUTION DE LA REPARTITION DES OPERATIONS SUR LES STATIONS
+
+void attributionRepartition(Repartition* repartition, const Exclusion exclusion[], int nbExclusions)
+{
+    int station = 1;
+    for (int i = 0; i < nbExclusions; i++) {
+        if (repartition->station[exclusion[i].op1 - 1] == -1) {
+            repartition->station[exclusion[i].op1 - 1] = station;
+        }
+
+        if (repartition->station[exclusion[i].op2 - 1] == -1) {
+            repartition->station[exclusion[i].op2 - 1] = station;
+        }
+
+        station = (station % NB_STATIONS_MAX) + 1;
+    }
+}
+
+
 
 
 
