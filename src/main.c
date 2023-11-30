a8bb4d06ef603a42b0525f37853f1b5525eb8300
Annabelle Leoni <annabelle.leoni@edu.ece.fr>
2023-11-30 13:56:49 +0100

    les contraintes sont différenciées entre "pair" et "impair". Le premier élément est pair (1-1=0), second impair... En mettant premier élément pair avec impaire... on obtient les couples de contraintes (comme dans le fichier.txt)
    Co-authored-by: Alara Tanguy <alara.tanguy@edu.ece.fr>

--- src/main.c
+++ src/main.c
@@ -35,6 +35,8 @@
 //structure stockage
 //n stations (donc il faut que le nombre de bloc soit en allocation dynamique aussi?)
 
+
+///*
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
@@ -100,6 +102,34 @@ void Afficher_Tableau (int tableau[100], int N) //DEBUG
     printf("\n");
 }
 
+ int contraintes (int tableau[100], int N){
+    int paire_contraintes;
+    int pair;
+    int impair;
+
+    for (int i = -1; i< N-1; i++){
+
+        paire_contraintes = i+1;
+        //printf("l'élément est : %d\n", paire_contraintes);
+        //printf("impaire c'est : %d\n", paire_contraintes%2);
+        //printf("\n");
+
+
+        if (paire_contraintes%2 == 0){
+            pair = tableau[paire_contraintes];
+            printf("voici la paire contrainte %d ", pair);
+        }
+
+        if (paire_contraintes%2 != 0){
+            impair = tableau[paire_contraintes];
+            printf("%d\n", impair);
+            //printf("Voici la paire contrainte: %d %d\n", tableau[paire_contraintes], tableau[paire_contraintes--]);
+        }
+
+    }
+    return paire_contraintes;
+}
+
 int main()
 {
     char nom_fichier[100];
@@ -111,146 +141,12 @@ int main()
 
     lire_fichier(nom_fichier, tableau, &N);
 
-    Afficher_Tableau(tableau, N);
-
+   // Afficher_Tableau(tableau, N);
+    contraintes(tableau, N);
     Trouver_Min_Max(tableau, N);
 
     return 0;
 }
 //prochaine étape créer un bloc 1 (le premier) qui va du min au max
 //faire en sorte que le tableau(?) soit en allocation dynamique
-
-
-
-/*
- * #include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
-
-typedef struct {
-    int* operations;
-    int nombres;
-} Station;
-
-void lire_fichier(char* nom_fichier, int tableau[100], int** exclusion, int* N) {
-    FILE *fichier = fopen(nom_fichier, "r");
-
-    if (!fichier) {
-        perror("Fichier non trouvé");
-        exit(EXIT_FAILURE);
-    }
-
-    int i = 0;
-    while (fscanf(fichier, "%d", &tableau[i]) != EOF) {
-        exclusion[i] = calloc(*N, sizeof(int));
-        i++;
-    }
-
-    *N = i;
-    fclose(fichier);
-}
-
-void liberer_exclusion(int** exclusion, int N) {
-    for (int i = 0; i < N; i++) {
-        free(exclusion[i]);
-    }
-    free(exclusion);
-}
-
-void Trouver_Min_Max(int tableau[100], int** exclusion, int N) {
-    int minV = INT_MAX, maxV = INT_MIN;
-
-    // Exclusion des couples de valeurs ? (1, 4), (1,17 ), ...
-    for (int i = 0; i < N; i++) {
-        if (!exclusion[i][i]) {
-            if (tableau[i] < minV) minV = tableau[i]; // Si la valeur est inférieure au min, on la remplace par la valeur actuelle
-            if (tableau[i] > maxV) maxV = tableau[i]; // Si la valeur est supérieure au max, on la remplace par la valeur actuelle
-
-            //Pour chaque valeur, on regarde si elle est égale à une autre valeur
-            //PAS LOGIQUE PTN
-            for (int j = i + 1; j < N; j++) {
-                if (tableau[j] == tableau[i]) { // Si la valeur est égale à la valeur actuelle
-                    exclusion[j][i] = exclusion[i][j] = 1; // On l'exclut
-                }
-            }
-        }
-    }
-
-    printf("Le minimum est : %d\n", minV);
-    printf("Le maximum est : %d\n", maxV);
-}
-
-/// https://www.youtube.com/watch?v=6mlp13UGfUM&ab_channel=HassanELBAHI
-void Repartir_Operations(int tableau[100], int** exclusion, int N) {
-    Station stations[3] = {{0}, {0}, {0}};
-
-    for (int i = 0; i < N; i++)
-    {
-        if (!exclusion[i][i]) {
-            int minStation = 0;
-            for (int j = 1; j < 3; j++)
-            {
-                if (stations[j].nombres < stations[minStation].nombres) // Si le nombre d'opérations de la station j est inférieur au nombre d'opérations de la station minStation
-                {
-                    minStation = j; // On remplace minStation par j
-                }
-            }
-
-
-            /// ALLOCATION DYNAMIQUE DU TABLEAU DE LA STATION MINSTATION
-            /// // (http://sdz.tdct.org/sdz/ableaux-pointeurs-et-allocation-dynamique.html (Fonction))
-
-            stations[minStation].operations = realloc(stations[minStation].operations,
-                                                      (stations[minStation].nombres + 1) * sizeof(int));
-            // On augmente la taille du tableau de la station minStation de 1 ==> FONCTION SUPER UTILE POUR NOUS, NE PAS OUBLIER LE +1
-
-            stations[minStation].operations[stations[minStation].nombres++] = tableau[i];
-            // On ajoute la valeur à la station minStation et on incrémente le nombre d'opérations de la station minStation afin de pouvoir ajouter une nouvelle valeur
-            // CHERCHER LA DIFFERENCE
-
-        }
-    }
-
-    for (int i = 0; i < 3; i++) {
-        printf("Station %d : ", i + 1);
-        for (int j = 0; j < stations[i].nombres; j++) {
-            printf("%d ", stations[i].operations[j]);
-        }
-        printf("\n");
-
-        free(stations[i].operations);
-    }
-}
-
-void Afficher_Tableau(int tableau[100], int N) {
-    printf("Tableau : ");
-    for (int i = 0; i < N; i++) {
-        printf("%d ", tableau[i]);
-    }
-    printf("\n");
-}
-
-int main() {
-    char nom_fichier[100];
-    int tableau[100];
-    int** exclusion;
-    int N;
-
-    printf("Quel fichier voulez-vous lire ?\n");
-    scanf("%99s", nom_fichier);
-
-    exclusion = (int*)malloc(100 * sizeof(int));
-
-    lire_fichier(nom_fichier, tableau, exclusion, &N);
-
-    Afficher_Tableau(tableau, N);
-
-    Trouver_Min_Max(tableau, exclusion, N);
-
-    Repartir_Operations(tableau, exclusion, N);
-
-    liberer_exclusion(exclusion, N);
-
-    return 0;
-}
- */
+//*/
