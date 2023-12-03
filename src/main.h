27af8e3611fd433b074a96eb7d5adf237c5af6a7
Alara Tanguy <alara.tanguy@edu.ece.fr>
2023-12-03 13:06:16 +0100

    Séparation des codes et du main pour la fusion
    Co-authored-by: annabelle-lni <114351870+annabelle-lni@users.noreply.github.com>
    Co-authored-by: Alara Tanguy <alara.tanguy@edu.ece.fr>
    Co-authored-by: Felix CADENE <felixcadene0@gmail.com>

---
+++ src/main.h
@@ -0,0 +1,12 @@
+#ifndef OPTIMISATION_D_UNE_LIGNE_D_ASSEMBLAGE_ING2_TG_2023_2024_5_6_MAIN_H
+#define OPTIMISATION_D_UNE_LIGNE_D_ASSEMBLAGE_ING2_TG_2023_2024_5_6_MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include "Bloc.h"
+
+int exclusion();
+
+
+#endif //OPTIMISATION_D_UNE_LIGNE_D_ASSEMBLAGE_ING2_TG_2023_2024_5_6_MAIN_H
